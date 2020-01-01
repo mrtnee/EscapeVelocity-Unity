b48bdb5178f99f8f24a7bdd83dbadd610cbf7657
Martin <martin.preradovic@hotmail.com>
2020-01-01 12:08:38 +0100

    first commit

---
+++ Assets/Scripts/FlightController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlightController : MonoBehaviour
+{
+    [SerializeField]
+    float flightSpeed = 30;
+    [SerializeField]
+    float rollSpeed = 30;
+    [SerializeField]
+    float pitchSpeed = 30;
+
+    bool up;
+    bool down;
+    bool right;
+    bool left;
+    bool accelerate;
+    bool shoot;
+
+    float dt;
+
+    Transform transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dt = Time.deltaTime;
+
+
+
+        transform.Rotate(Input.GetAxis("Vertical") * dt * pitchSpeed, 0.0f, -Input.GetAxis("Horizontal") * dt * rollSpeed);
+        transform.position += transform.forward * dt * flightSpeed;
+    }
+
+    // Update is called once per pyhisics update
+    private void FixedUpdate()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/FollowingCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 defaultDistance = new Vector3(0f, 2f, -10f);
+    [SerializeField] float distanceDamp = 0.5f;
+    [SerializeField] float rotationDamp = 0.5f;
+
+    Vector3 velocity = Vector3.one;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 toPos = target.position + (target.rotation * defaultDistance);
+        Vector3 currPos = Vector3.SmoothDamp(transform.position, toPos, ref velocity, distanceDamp);
+        transform.position = currPos;
+
+        transform.LookAt(target, target.up);
+    }
+}
