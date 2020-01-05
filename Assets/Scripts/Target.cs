813a4dfa48e8d94f787f451985dc446a4f4ce4fe
Niki Bizjak <drobilc@gmail.com>
2020-01-05 16:04:32 +0100

    Add explosion when player is hit

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+
+    public ParticleSystem explosion;
+
     public void Die() {
         // This function will be called when player is shot by their opponent
-        Debug.Log("HIT!");
+
+        // Play the explosion particle system
+        explosion.Play();
     }
 
 }
