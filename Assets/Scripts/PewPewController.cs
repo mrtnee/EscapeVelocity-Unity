f1e5f16d6a4e6b2ee56d2c8bc76aa33443a24a95
Niki Bizjak <drobilc@gmail.com>
2020-01-05 12:55:56 +0100

    Add shooting controller to both aircraft prefabs

---
+++ Assets/Scripts/PewPewController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PewPewController : MonoBehaviour {
+
+    public KeyCode shoot;
+
+    void Update() {
+        if (Input.GetKey(shoot)) {
+            Shoot();
+        }
+    }
+
+    void Shoot() {
+        RaycastHit hit;
+        if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out hit, 100000)) {
+            // We have hit something, check if the object that was shot is in group aircraft
+            if (hit.transform.tag.Equals("aircraft")) {
+                Debug.Log(hit.transform.name);
+            }
+        }
+    }
+
+}
