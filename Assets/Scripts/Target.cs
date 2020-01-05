3102b9b543ffe6855dc86cd79da0acfd49b8a644
Niki Bizjak <drobilc@gmail.com>
2020-01-05 13:08:18 +0100

    Add game manager to manage current game state

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KapowController : MonoBehaviour
+{
+    void die() {
+        // This function will be called when player is shot by their opponent
+    }
+
+}
