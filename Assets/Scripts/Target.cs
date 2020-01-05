80d4b901a4e633382dd8f68349b4e86282f6cc3f
Niki Bizjak <drobilc@gmail.com>
2020-01-05 14:54:39 +0100

    Add target script to aircraft prefabs

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KapowController : MonoBehaviour
+public class Target : MonoBehaviour
 {
     void die() {
         // This function will be called when player is shot by their opponent
