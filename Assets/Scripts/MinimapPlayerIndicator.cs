2216ab070317fefcf2ae314763b1ab0f56e57fc6
Martin <martin.preradovic@hotmail.com>
2020-01-06 18:15:13 +0100

    Add player indicators on the minimap

---
+++ Assets/Scripts/MinimapPlayerIndicator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapPlayerIndicator : MonoBehaviour
+{
+    public Transform player;
+    Vector3 p;
+    Vector3 r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdatePosition();
+        UpdateRotation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePosition();
+        UpdateRotation();
+    }
+
+    private void UpdatePosition()
+    {
+        p = player.position;
+        p.Set(p.x, transform.position.y, p.z);
+        transform.position = p;
+    }
+
+    private void UpdateRotation()
+    {
+        r = player.eulerAngles;
+        r.Set(transform.eulerAngles.x, r.y, transform.eulerAngles.z);
+        transform.eulerAngles = r;
+    }
+}
